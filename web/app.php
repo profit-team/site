d42e342d56abc12a3e87bfa0c0dcc15852873858
d1gi <digital_boy@mail.ru>
2015-05-28 00:38:54 +0600

    all web folder

---
+++ web/app.php
@@ -0,0 +1,26 @@
+<?php
+define('START_TIME', microtime(true));
+define('START_MEMORY', memory_get_usage());
+
+require_once __DIR__.'/../var/bootstrap.php.cache';
+require_once __DIR__.'/../app/AppKernel.php';
+//require_once __DIR__.'/../app/AppCache.php';
+
+define('APPKERNEL_DEBUG', true);
+//define('APPKERNEL_DEBUG', false);
+\Profiler::enable();
+
+use Symfony\Component\HttpFoundation\Request;
+
+$kernel = new AppKernel('prod', APPKERNEL_DEBUG);
+$kernel->loadClassCache();
+//$kernel = new AppCache($kernel);
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
+
+if ($response->headers->contains('content-type', 'text/html; charset=UTF-8') and !$response->isRedirection()) {
+//    \Profiler::render(true);
+//    \Profiler::dump(get_included_files(), dirname(__DIR__), '\var\cache\\');
+}
