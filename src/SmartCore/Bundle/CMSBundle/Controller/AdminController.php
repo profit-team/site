ff4dbed605a9935b5b8500b34aacf8aca22173f3
d1gi <digital_boy@mail.ru>
2015-05-30 20:00:15 +0600

    cms update

--- src/SmartCore/Bundle/CMSBundle/Controller/AdminController.php
+++ src/SmartCore/Bundle/CMSBundle/Controller/AdminController.php
@@ -10,6 +10,7 @@ use Symfony\Component\Finder\Finder;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Kernel;
 
 class AdminController extends Controller
 {
@@ -45,6 +46,150 @@ class AdminController extends Controller
         return $this->render('CMSBundle:Admin:not_found.html.twig', []);
     }
 
+    /**
+     * @return Response
+     */
+    public function reportsAction()
+    {
+        $system = [
+            'php' => $this->getPhpSettings(),
+            'platform' => $this->getPlatformInfo(),
+        ];
+
+        return $this->render('CMSBundle:Admin:reports.html.twig', [
+            'system' => $system,
+        ]);
+    }
+
+    /**
+     * Получить информацию о платформе.
+     *
+     * @return array
+     */
+    protected function getPlatformInfo()
+    {
+        $db = $this->get('database_connection');
+
+        $data = [];
+        $data[] = [
+            'title' => 'Smart Core CMS version',
+            'value' => 'v0.5',
+            'required' => '',
+            'recomended' => '',
+            'hint' => '',
+            'warning' => 0,
+        ];
+        $isDebug = $this->get('kernel')->isDebug() ? 'true' : 'false';
+        $data[] = [
+            'title' => 'Symfony2 Framework version',
+            'value' => Kernel::VERSION.' (debug = '.$isDebug.')',
+            'required' => '',
+            'recomended' => '',
+            'hint' => '',
+            'warning' => 0,
+        ];
+        $data[] = [
+            'title' => 'Database server',
+            'value' => $db->query('SHOW VARIABLES LIKE "%version_comment%"')->fetchObject()->Value.' version '.$db->query('SELECT version() AS version')->fetchObject()->version,
+            'required' => 'MySQL 5.1',
+            'recomended' => 'MySQL 5.5+ or PostgreSQL 9.3+',
+            'hint' => '',
+            'warning' => 0,
+        ];
+        $data[] = [
+            'title' => 'Web Server',
+            'value' => $_SERVER['SERVER_SOFTWARE'].' ('.php_sapi_name().')',
+            'required' => '',
+            'recomended' => '',
+            'hint' => '',
+            'warning' => 0,
+        ];
+        return $data;
+    }
+
+    /**
+     * Получить настрйоки PHP.
+     *
+     * @return array
+     */
+    protected function getPhpSettings()
+    {
+        $data = [];
+        $data[] = [
+            'title' => 'PHP Version',
+            'value' => phpversion().' ('.php_uname('m').')',
+            'required' => '5.4.1',
+            'recomended' => '5.5.9+',
+            'hint' => '',
+            'warning' => 0,
+        ];
+        $data[] = [
+            'title' => 'Memory Limit',
+            'value' => ini_get('memory_limit'),
+            'required' => '128M',
+            'recomended' => '256M',
+            'hint' => '',
+            'warning' => 0,
+        ];
+        $data[] = [
+            'title' => 'Safe Mode',
+            'value' => ini_get('safe_mode') ? 'On' : 'Off',
+            'required' => 'Off',
+            'recomended' => '',
+            'hint' => '',
+            'warning' => 0,
+        ];
+        $data[] = [
+            'title' => 'Display Errors',
+            'value' => ini_get('display_errors') ? 'On' : 'Off',
+            'required' => 'Off',
+            'recomended' => '',
+            'hint' => '',
+            'warning' => 0,
+        ];
+        $data[] = [
+            'title' => 'Magic Quotes',
+            'value' => ini_get('magic_quotes_gpc') ? 'On' : 'Off',
+            'required' => 'Off',
+            'recomended' => '',
+            'hint' => '',
+            'warning' => 0,
+        ];
+        $data[] = [
+            'title' => 'Register Globals',
+            'value' => ini_get('register_globals') ? 'On' : 'Off',
+            'required' => 'Off',
+            'recomended' => '',
+            'hint' => '',
+            'warning' => 0,
+        ];
+        $data[] = [
+            'title' => 'Output Buffering',
+            'value' => ((bool)ini_get('output_buffering')) ? 'On' : 'Off',
+            'required' => 'On',
+            'recomended' => '',
+            'hint' => '',
+            'warning' => 0,
+        ];
+        $data[] = [
+            'title' => 'Mbstring Enabled',
+            'value' => extension_loaded('mbstring') ? 'Yes' : 'No',
+            'required' => 'Yes',
+            'recomended' => '',
+            'hint' => '',
+            'warning' => 0,
+        ];
+        $data[] = [
+            'title' => 'Upload max filesize',
+            'value' => ini_get('upload_max_filesize'),
+            'required' => '4M',
+            'recomended' => '20M',
+            'hint' => '',
+            'warning' => 0,
+        ];
+        return $data;
+    }
+    
     /**
      * Renders Elfinder.
      *
